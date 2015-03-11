bf2917432d2cb86d9c3a4330ffa9f2ccd5cd0b81
DillonMarchese <dillon.marchese@gmail.com>
2015-03-11 08:56:38 -0500

    Dillon 12
    Changes finalized to the server browser and code stuff.

---
+++ Source/EmptySkies/BPFunctionLibrary.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EmptySkies.h"
+#include "BPFunctionLibrary.h"
+
+
+
+
+UFUNCTION(FUNC_BlueprintCallable, Category = "Dillon's Nodes")
+void SetSessionName(string SessionName)
+{
+
+}
---
+++ Source/EmptySkies/BPFunctionLibrary.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPTYSKIES_API UBPFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
